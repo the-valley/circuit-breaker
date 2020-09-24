3b9a7a1153ce142ee77d3c017dbced9b32613846
wangyong <autumnimagery@outlook.com>
2020-09-24 19:29:28 +0800

    窗口统计方式

--- src/main/java/com/wy/counter/SlidingWindowCounter.java
+++ src/main/java/com/wy/counter/SlidingWindowCounter.java
@@ -1,6 +1,8 @@
 package com.wy.counter;
 
+import java.util.Objects;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @author wy
@@ -11,7 +13,9 @@ public class SlidingWindowCounter {
     /**
      * 窗口队列，容量根据熔断器参数设定，如果程序执行成功则保存1，否则保存0
      */
-    private LinkedBlockingDeque<Integer> window;
+    private final LinkedBlockingDeque<Integer> window;
+
+    private final LongAdder failureCounter = new LongAdder();
 
     public SlidingWindowCounter(Integer windowSize) {
         this.window = new LinkedBlockingDeque<>(windowSize);
@@ -19,27 +23,36 @@ public class SlidingWindowCounter {
 
     /**
      * 窗口移动
+     *
      * @param executeStatus 执行状态
-     * @return 是否成功添加
      */
-    public boolean add(ExecuteStatusEnum executeStatus) {
+    public synchronized void add(ExecuteStatusEnum executeStatus) {
         // 添加元素前先判断队列是否已满，满需要把最开始入列的元素删除
-        if (window.offer(executeStatus.getCode())) {
-            window.poll();
-            return window.offer(executeStatus.getCode());
+        final boolean offer = window.offer(executeStatus.getCode());
+        // 如果添加成功且添加的状态为失败，那失败数加1
+        if (offer) {
+            if (ExecuteStatusEnum.FAIL.equals(executeStatus)) {
+                failureCounter.increment();
+            }
+        } else {
+            final Integer poll = window.poll();
+            // 如果取出的第一个元素不为null，且状态为失败那失败数减1
+            if (!Objects.isNull(poll) && ExecuteStatusEnum.FAIL.getCode().equals(poll)) {
+                failureCounter.decrement();
+            }
+            // 添加新的元素
+            final boolean offer1 = window.offer(executeStatus.getCode());
+            if (offer1 && ExecuteStatusEnum.FAIL.equals(executeStatus)) {
+                failureCounter.increment();
+            }
         }
-        return false;
     }
 
     /**
      * 计算队列中失败的个数
      */
-    public Integer countFailure() {
-        int sum = 0;
-        for (Integer item : window) {
-            sum += item;
-        }
-        return sum;
+    public synchronized Integer countFailure() {
+        return failureCounter.intValue();
     }
 
     /**
